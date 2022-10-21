2e9900548be345f2364e31329b0545dfc0df778b
Diego Casillas <52338302+realdiegopoptart@users.noreply.github.com>
2022-10-21 11:09:50 -0500

    started brain instance (first commit)

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#  __ ______ __   ___   ___          
+# /_ |____  / /  / _ \ / _ \   /\    
+#  | |   / / /_ | (_) | (_) | /  \   
+#  | |  / / '_ \ > _ < > _ < / /\ \  
+#  | | / /| (_) | (_) | (_) / ____ \ 
+#  |_|/_/  \___/ \___/ \___/_/    \_\
+#     CORONA DEL SOL HIGH SCHOOL
+#          VRC SPINUP 2022                        
+                                    
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("17688A")
