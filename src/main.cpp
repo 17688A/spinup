a11c128f240164dbe138d41e34ec106fbddfd72f
Diego Casillas <52338302+realdiegopoptart@users.noreply.github.com>
2022-10-21 13:17:04 -0700

    added comments for documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+/**
+*  __ ______ __   ___   ___          
+* /_ |____  / /  / _ \ / _ \   /\    
+*  | |   / / /_ | (_) | (_) | /  \   
+*  | |  / / '_ \ > _ < > _ < / /\ \  
+*  | | / /| (_) | (_) | (_) / ____ \ 
+*  |_|/_/  \___/ \___/ \___/_/    \_\
+*     CORONA DEL SOL HIGH SCHOOL
+*
+*/
+
 #include "main.h"
 
 /**
@@ -74,20 +85,23 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	// Controller master is the instance that run the code
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_mtr(1);
 	pros::Motor right_mtr(2);
+	// 2 lines above are the motor ports
 
 	while (true) {
+		// debugging
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+		int left = master.get_analog(ANALOG_LEFT_Y); // set the analog key to the variable
+		int right = master.get_analog(ANALOG_RIGHT_Y); // set the analog key to the variable
 
-		left_mtr = left;
-		right_mtr = right;
+		left_mtr = left; // set the motor to the variable
+		right_mtr = right; // set the motor to the variable
 
-		pros::delay(20);
+		pros::delay(20); // dont hog the cpu
 	}
 }
